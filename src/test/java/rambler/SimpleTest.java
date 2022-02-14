80a19d8d748458963913b5852a61752df35d7a2c
0yanna <yannaosodoeva@outlook.com>
2022-02-14 20:34:36 +0300

    added SimpleTest

---
+++ src/test/java/rambler/SimpleTest.java
@@ -0,0 +1,16 @@
+package rambler;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SimpleTest {
+    @Test
+     void selenideSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("selenide.org"));
+    }
+}
